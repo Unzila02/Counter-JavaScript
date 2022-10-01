79bdd8b2f9b6dc5494fa1a1c5c3e1214329b88f6
Unzila02 <unzilamaqsood123@gmail.com>
2022-10-01 06:09:47 -0700

    update javascript counter increase and decrease value

---
+++ index.js
@@ -0,0 +1,22 @@
+function increaseValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+
+}
+function decreaseValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+	
+}
+	
+	function resetvalue() {
+
+		document.getElementById('number').value = '0'
+
+}
